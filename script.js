cdd43dade73889bf1542b3946753273d9762b181
Michał Żuk <michal.zuk30601@gmail.com>
2022-02-26 17:53:24 +0100

    Dodanie przysicków zmieniających widoczność tabel.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//alert("Hello");
+function html_table()
+{
+    document.getElementById("html_table").style.display="table";
+    document.getElementById("html_table_h").style.display="block";
+    document.getElementById("css_table").style.display="none";
+    document.getElementById("css_table_h").style.display="none";
+    document.getElementById("html_table_but").style.border="5px solid green";
+    document.getElementById("css_table_but").style.border="";
+    
+
+}
+
+function css_table()
+{
+    document.getElementById("css_table").style.display="table";
+    document.getElementById("css_table_h").style.display="block";
+    document.getElementById("html_table").style.display="none";
+    document.getElementById("html_table_h").style.display="none";
+    document.getElementById("css_table_but").style.border="5px solid green";
+    document.getElementById("html_table_but").style.border="";
+
+}
