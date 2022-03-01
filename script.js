fd5349848182dafd87e7b1d00b6fe5b350be33ef
Michał Żuk <michal.zuk30601@gmail.com>
2022-03-01 14:35:30 +0100

    dodanie opisu funkcji CSS

--- script.js
+++ script.js
@@ -2,21 +2,47 @@ function html_table()
 {
     document.getElementById("html_table").style.display="table";
     document.getElementById("html_table_h").style.display="block";
+    document.getElementById("html_table_but").style.border="5px solid green";
+
     document.getElementById("css_table").style.display="none";
     document.getElementById("css_table_h").style.display="none";
-    document.getElementById("html_table_but").style.border="5px solid green";
     document.getElementById("css_table_but").style.border="";
+
+    
+    document.getElementById("cssf_table").style.display="none";
+    document.getElementById("cssf_table_h").style.display="none";
+    document.getElementById("cssf_table_but").style.border="";
     
 
 }
 
 function css_table()
 {
-    document.getElementById("css_table").style.display="table";
-    document.getElementById("css_table_h").style.display="block";
     document.getElementById("html_table").style.display="none";
     document.getElementById("html_table_h").style.display="none";
-    document.getElementById("css_table_but").style.border="5px solid green";
     document.getElementById("html_table_but").style.border="";
 
+    document.getElementById("css_table").style.display="table";
+    document.getElementById("css_table_h").style.display="block";
+    document.getElementById("css_table_but").style.border="5px solid green";
+    
+    document.getElementById("cssf_table").style.display="none";
+    document.getElementById("cssf_table_h").style.display="none";
+    document.getElementById("cssf_table_but").style.border="";
+
+}
+
+function cssf_table()
+{
+    document.getElementById("html_table").style.display="none";
+    document.getElementById("html_table_h").style.display="none";
+    document.getElementById("html_table_but").style.border="";
+
+    document.getElementById("css_table").style.display="none";
+    document.getElementById("css_table_h").style.display="none";
+    document.getElementById("css_table_but").style.border="";
+    
+    document.getElementById("cssf_table").style.display="table";
+    document.getElementById("cssf_table_h").style.display="block";
+    document.getElementById("cssf_table_but").style.border="5px solid green";
 }
